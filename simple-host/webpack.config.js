e9e40eebbac6a34409d6d2656fffad0ed4130d2f
damar <damar@gitlab.lab>
2023-01-02 15:54:28 +0700

    fix webpack.config.js

--- simple-host/webpack.config.js
+++ simple-host/webpack.config.js
@@ -25,11 +25,5 @@ module.exports = (webpackConfigEnv, argv) => {
       }),
     ],
   },
-  {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    },
   );
 };
